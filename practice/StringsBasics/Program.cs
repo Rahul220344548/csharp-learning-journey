5bc88264badf7d6e430e1bfa4e91dc3b5dcee8f2
Rahul Vasantlal <220344548@aston.ac.uk>
2026-02-24 13:24:26 +0000

    Initial commit

---
+++ practice/StringsBasics/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet watch run  - LIVE RUN
+// CTRL + C in terminal - EXIT
+
+int fahrenheit = 94;
+
+decimal temperature = (fahrenheit - 32) * 5.0m / 9;
+Console.WriteLine($"The temperature in Celsius is {temperature} degrees.");
