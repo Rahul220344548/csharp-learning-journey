069f07f7f02b1fdf7cd7dbffba6a07df5eaf0125
Rahul Vasantlal <220344548@aston.ac.uk>
2026-02-24 13:43:14 +0000

    add graded assignments project

---
+++ practice/Guided-Projects/GP-Student-Grades/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int currentAssignments = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
