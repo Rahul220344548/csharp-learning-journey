c8788d591bc88138852e54d7297b1095a32fb4d3
Rahul Vasantlal <220344548@aston.ac.uk>
2026-03-10 18:45:14 +0000

    practice: explore Random class and methods

---
+++ practice/TestProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+Random dice2 = new Random();
+int roll2 = dice2.Next(1, 7);
+Console.WriteLine(roll2);
